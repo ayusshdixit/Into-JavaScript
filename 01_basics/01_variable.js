bc61cb33fde1483172957cfc1eb8a353dd94a039
ayusshdixit <samratdixit05@gmail.com>
2025-06-06 14:16:10 +0000

    contain config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const CustomerId = 12356;
+let CustomerName="Aayush";
+ var AccountNumber="23456";
+ accountCity="Lucknow";
+ let Accountstate;
+
+
+
+console.table([CustomerId, CustomerName, AccountNumber,accountCity,Accountstate]) ;
