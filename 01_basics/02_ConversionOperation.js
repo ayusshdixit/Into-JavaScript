543b31668691f37b0e1ed5825bae48e78f959106
ayusshdixit <samratdixit05@gmail.com>
2025-06-07 13:35:38 +0000

    conversion of datatype

--- 01_basics/02_ConversionOperation.js
+++ 01_basics/02_ConversionOperation.js
@@ -1,15 +1,24 @@
 let score = "33ABC"
-console.log ( typeof score)
+//console.log ( typeof score)
 let valueInNumber=Number(score)
-console.log ( typeof valueInNumber )
-console.log(valueInNumber )
+//console.log ( typeof valueInNumber )
+//console.log(valueInNumber )
 
 //33=> 33
 //"33" => 33
 //"33abc" => NaN
 // true => 1 false => 0
 
-let logedin =1;
- let booleanlogedin ean(Boolean);
- console.log(booleanIsBoolean);
+let loggedin =1;
+
+
+
+let myLoggedin = Boolean(loggedin)
+//console.log(myLoggedin);
+//empty => false 
+//"" => true
+let somenumber = 33;
+let strsomenumber = String(somenumber)
+ console.log(typeof strsomenumber);
+ console.log(strsomenumber);
  
