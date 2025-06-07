9674c4e877836c943094f613a974d8ee020591a8
ayusshdixit <samratdixit05@gmail.com>
2025-06-07 15:39:36 +0000

    datatype-summary done

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,33 @@
+//primitive datatype 
+// types=> 7 
+// String , null , undefined , Number , Bigint , Symbol , Boolean 
+
+const score =300; // Number it can be 100.3
+const LoggedIn = false ; // its a Boolean 
+const str = " samrat" // its a string 
+ let useremail ; // its undefined 
+ // symbod 
+ const id = Symbol(123);
+ const userid = Symbol(123);
+ console.log(userid===id);
+ console.log(id);
+ 
+ //refernce datatype 
+
+ //Array , Objects , Functions 
+ 
+ const heros = ["shaktiman","nagraj","doga"]  
+    // ARRAY 
+    // these are objects 
+    let myObj ={
+        name : "samrat",
+        age : 19 ,
+    }
+    // functions 
+     const myfunction = function() { 
+
+        console.log("samart");
+        
+    }
+    // use typeof to know the datatype of variables 
+    
