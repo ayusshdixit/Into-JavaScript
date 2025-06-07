f86e998d9405579bf75867a47436b9a28d64555d
ayusshdixit <samratdixit05@gmail.com>
2025-06-07 15:03:13 +0000

    camparison is done

--- 01_basics/02_ConversionOperation.js
+++ 01_basics/02_ConversionOperation.js
@@ -21,4 +21,27 @@ let somenumber = 33;
 let strsomenumber = String(somenumber)
  console.log(typeof strsomenumber);
  console.log(strsomenumber);
+
+
+ //*********************Operations****************************************
+
+ //basics arthmetic Oper5ations 
+
+ let str1 = "hello"
+ let str2 = " samrat" 
+
+ let str3 = str1+ str2;
+//console.log(str3);
  
+// console.log ("1"  + 2 )
+// console.log("2" + 3) ;
+// console.log(1 + 2 + "3" ) ;
+// console.log ( (3+4) * 5 % 3);  //its a good code practise
+// console.log( +true);
+console.log(+"");   //=> 0
+console.log(+true);//=> 1
+
+let gamecounter=100;
+gamecounter++
+console.log(gamecounter);
+
