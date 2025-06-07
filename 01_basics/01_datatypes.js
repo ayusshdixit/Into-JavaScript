b36381a446f30727168fefa41c6c58687f10d090
ayusshdixit <samratdixit05@gmail.com>
2025-06-07 12:07:33 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version 
+// alert will not used as we are using node js not browser
+
+
+console.log("Samrat");// this is a String
+let name = " samrat"
+let age = 18; //number datatype
+let isloggedin= false //boolean
+
+//null => object // its a standalone value 
+//undefined => undefined 
+console.log(typeof null);
+console.log(typeof undefined);
+//symbol => to find uniqueness 
+// these all are primitive datatype ;
+
