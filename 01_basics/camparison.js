13232f5b8c20d4c2762ca14e1665be6da38439d2
ayusshdixit <samratdixit05@gmail.com>
2025-06-07 15:02:38 +0000

    camparison is done

---
+++ 01_basics/camparison.js
@@ -0,0 +1,7 @@
+// 
+console.log ( null > 0);
+console.log ( null == 0);
+console.log ( null >= 0 ) ; // here the null get converted into 0 
+// undefined gives false value applied in all these three operations 
+
+console.log ( "2" === 2 ) ; // now it does not give the same value as before nnow  will check its datatype also
