0d01e3372e9c3dde50e6efb3ff3f8a45cf5960e8
ayusshdixit <samratdixit05@gmail.com>
2025-06-07 13:21:22 +0000

    conversiondatatype is done

---
+++ 01_basics/02_ConversionOperation.js
@@ -0,0 +1,15 @@
+let score = "33ABC"
+console.log ( typeof score)
+let valueInNumber=Number(score)
+console.log ( typeof valueInNumber )
+console.log(valueInNumber )
+
+//33=> 33
+//"33" => 33
+//"33abc" => NaN
+// true => 1 false => 0
+
+let logedin =1;
+ let booleanlogedin ean(Boolean);
+ console.log(booleanIsBoolean);
+ 
