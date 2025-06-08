738da0786d885c414c1de6a760c47a0b895d0e49
ayusshdixit <samratdixit05@gmail.com>
2025-06-08 16:04:48 +0000

    date and time is done

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,34 @@
+// date 
+let myDate = new Date();
+console.log(myDate);
+// 2025-06-08T15:25:52.651Z
+
+console.log(myDate.toDateString());
+//Sun Jun 08 2025
+
+console.log(myDate.toISOString());
+// 2025-06-08T15:25:52.651Z
+
+console.log(myDate.toJSON());
+// 2025-06-08T15:25:52.651Z
+
+console.log(myDate.toLocaleString());
+// 6/8/2025, 3:25:52 PM
+
+console.log(myDate.toString());
+//Sun Jun 08 2025 15:25:52 GMT+0000 (Coordinated Universal Time)
+console.log(typeof myDate);
+//object
+
+let myCreateDate = new Date( 2023, 0, 23)
+
+console.log(myCreateDate.toDateString());
+//Mon Jan 23 2023
+let myCreatedDate = new Date( 2023, 0, 23,5,3)
+
+console.log(myCreatedDate.toLocaleString());
+// 1/23/2023, 5:03:00 AM
+
+
+let timeStamp = new Date.new();
+log
