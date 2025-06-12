273a9304bde001e9fc0cabe49cae14ee44b2c6d5
ayusshdixit <samratdixit05@gmail.com>
2025-06-12 14:15:42 +0000

    iife is done

---
+++ 01_basics/02_basics/03_basics/04_iffe.js
@@ -0,0 +1,9 @@
+ (function chaiexe (){
+    console.log('chai is done ');
+    
+ }) ();
+      
+
+( function chaiaurdedo (name){
+    console.log(`this is chai ${name}`);
+}) ('aayush');  // these are named iffe
